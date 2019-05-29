45c5b016aa55de3c0194e38dad391b1049256939
JasonCB083 <jason.cllr@gmail.com>
2019-05-29 17:01:25 +0200

    2 deploy

---
+++ public/precache-manifest.6041576b3bb22416721e6383607a67fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb8628e8d736cc1e93c2",
+    "url": "/static/css/main.e1b7f46d.chunk.css"
+  },
+  {
+    "revision": "cb8628e8d736cc1e93c2",
+    "url": "/static/js/main.1665b6dc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0de8aa2884794c6cdade",
+    "url": "/static/js/2.bc0f4eb4.chunk.js"
+  },
+  {
+    "revision": "5844124841d2cf9e37afc72aefbd66ab",
+    "url": "/index.html"
+  }
+];
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.508f33feef042a33f411732d100581df.js"
+  "/precache-manifest.6041576b3bb22416721e6383607a67fb.js"
 );
 
 workbox.clientsClaim();
